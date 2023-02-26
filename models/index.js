d8544aa760e927bd46170d8d17bf62335c44b5c7
dorsariahi <dorsariahiasl@gmail.com>
2023-02-25 19:13:35 -0600

    updated index.js by adding the hasmany and blongs to funtions

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./user');
+const User = require('./User');
 const Post = require('./post');
 
 User.hasMany(Post, {
@@ -9,5 +9,19 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+})
 
 module.exports = { User, Post };
